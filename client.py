9427ad35e86d66d1019c05486633734139703ed4
Nicolas Kingreen <NicolasKingreen@gmail.com>
2023-02-27 11:59:44 +0300

    Functional chat

--- client.py
+++ client.py
@@ -1,28 +1,106 @@
 import socket
+import threading
+import os
+import re
+from time import sleep
+
+from util import format_addr
+
 
 SERVER_ADDRESS = ("192.168.160.28", 5050)
-
-def send(msg):
-    message = msg.encode("utf-8")
-    msg_length = len(message)
-    send_length = str(msg_length).encode("utf-8")
-    send_length += b' ' * (64 - len(send_length))
-    client.send(send_length)
-    client.send(message)
-    print(client.recv(2048).decode("utf-8"))
+MESSAGE_STARTS_WITH_TIMESTEMP_PATTERN = r'\d{2}:\d{2}:\d{2} *'
 
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(SERVER_ADDRESS)
-print(f"You are connected to {SERVER_ADDRESS}.\n * '!q' to disconnect\n * '!stop' to stop the server")
+class Client:
+    def __init__(self):
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connected = False
 
-getting_input = True
-while getting_input:
-    prompt = input(" > ")
+        self.update_thread = threading.Thread(target=self.update)
+        self.cli_thread = threading.Thread(target=self.draw_interface)
 
-    # client side prompt processing
-    if prompt == "!q":
-        getting_input = False
+        self.getting_input = False
+        self.got_new_message = False
 
-    send(prompt)
 
+        self.chat_history = []  # one entry = one message
+
+    def connect(self, addr):
+        self._socket.connect(addr)
+        self.connected = True
+
+        os.system('cls')
+        conn_str = f"You are connected to {format_addr(SERVER_ADDRESS)}."
+        self.chat_history.append(conn_str)
+        print(conn_str)
+
+        self.username = self._socket.recv(2048).decode()
+        usr_str = f"Your username: {self.username}"
+        self.chat_history.append(usr_str)
+        print(usr_str)
+
+    def start(self):
+
+        if self.connected:
+            self.update_thread.start()
+            self.cli_thread.start()
+
+            cmnds_str = " * '!q' to disconnect\n * '!stop' to stop the server"
+            self.chat_history.append(cmnds_str)
+            print(cmnds_str)
+
+            print(" > ", end='')
+            self.getting_input = True
+
+        while self.connected:
+            if self.getting_input:
+                self.get_input()
+
+        self._socket.close()
+
+    def get_input(self):
+        prompt = input()
+
+        # client side prompt processing
+        if prompt == "!q":
+            self.getting_input = False
+            self.connected = False
+
+        self.send(prompt)
+
+    def print_chat_history(self):
+        for message in self.chat_history:
+            print(message)
+
+    def send(self, message):
+        # message = msg.encode()
+        # msg_length = len(message)
+        # send_length = str(msg_length).encode("utf-8")
+        # send_length += b' ' * (64 - len(send_length))
+        self._socket.send(message.encode())
+
+    def update(self):
+        """
+        Processes server messages. If it starts with timestemp, then it's added to the client's history.
+        """
+        while self.connected:
+            if not self.got_new_message:
+                new_message = self._socket.recv(2048).decode()
+                if re.match(MESSAGE_STARTS_WITH_TIMESTEMP_PATTERN, new_message):
+                    self.chat_history.append(new_message)
+                    self.got_new_message = True
+
+    def draw_interface(self):
+        while self.connected:
+            if self.got_new_message:
+                os.system('cls')
+                self.print_chat_history()
+                print(" > ")  # TODO: fix prompt doesn't appear inline with input
+                self.got_new_message = False
+
+
+
+if __name__ == "__main__":
+    client = Client()
+    client.connect(SERVER_ADDRESS)
+    client.start()
--- server.py
+++ server.py
@@ -1,15 +1,19 @@
 import socket
 import threading
+import os
+
+from util import generate_username, format_addr, get_time
+
+# TODO: handle disconnetions, split packages in two (bytes and message itself), move server prints to logger?, send chat history to new client
 
 
 HOST_NAME = socket.gethostname()
 SERVER_HOST = socket.gethostbyname(HOST_NAME)
 SERVER_PORT = 5050
 SERVER_ADDRESS = (SERVER_HOST, SERVER_PORT)
-print(HOST_NAME, SERVER_HOST, SERVER_PORT)
 
-def generate_username():
-    pass
+os.system('cls')
+print(HOST_NAME, SERVER_HOST, SERVER_PORT)
 
 
 class Server:
@@ -17,48 +21,86 @@ class Server:
     def __init__(self):
         self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._socket.bind(SERVER_ADDRESS)
-        self.connections = []  # threads
+
+        self.connections = []
+        self.conn_threads = []
+
         self.running = False
+
+        self.chat_history = []
 
     def start(self):
         self._socket.listen()
-        print(f"[LISTENING] Server is listening on {SERVER_ADDRESS}")
+        print(f"[SERVER] Server is listening on {format_addr(SERVER_ADDRESS)}...")
         self.running = True
         while self.running:
+
             conn, addr = self._socket.accept()
+            self.connections.append((conn, addr))
+
             thread = threading.Thread(target=self.handle_client, args=(conn, addr))
-            self.connections.append(thread)
+            self.conn_threads.append(thread)
             thread.start()
-            print(f"[ACTIVE CONNECTION] {threading.active_count() - 1}")
+
+            # print(f"[SERVER] Total connections: {threading.active_count() - 1}")
+            # self.print_running_threads()
+
+        self._stop_threads()
+        self.close_all_connections()
 
     def stop(self):
+        print("[SERVER] Stopping server...")  # TODO: fix can't stop server cause accept() is blocking loop
         self.running = False
+
+    def _stop_threads(self):
         for thread in self.connections:
             thread.stop()
 
     def handle_client(self, conn, addr):
-        print(f"[NEW CONNECTION] {addr} connected.")
+        username = generate_username()
+        print(f"[SERVER] {username} ({format_addr(addr)}) connected.")
+        conn.send(username.encode())
         connected = True
         while connected:
 
             # message header
-            msg_length = conn.recv(64).decode("utf-8")
-            if msg_length:
-                msg_length = int(msg_length)
+            # msg_length = conn.recv(64).decode("utf-8")
+            # if msg_length:
+                # msg_length = int(msg_length)
 
-                msg = conn.recv(msg_length).decode("utf-8")
+            # msg = conn.recv(msg_length).decode("utf-8")
+            msg = conn.recv(2048).decode()
 
-                # server side prompt processing
-                if msg == "!q":
-                    print(f"[{addr}] Client disconnected.")
-                    connected = False
-                elif msg == "!stop":
-                    self.stop()
-                else:
-                    print(f"[{addr}] {msg}")
-                conn.send("Msg recieved".encode("utf-8"))
+            # server side prompt processing
+            if msg == "!q":
+                connected = False
+            elif msg == "!stop":
+                self.stop()
+            else:
+
+                # on success
+                chat_line = f"{get_time()} {username}: {msg}"
+                self.chat_history.append(chat_line)
+                print(chat_line)
+                for connection, address in self.connections:
+                    # print(f"[SERVER] Sending message ({chat_line}) to ({format_addr(address)})...")
+                    connection.send(chat_line.encode())
 
         conn.close()
+        print(f"[SERVER] {username} ({format_addr(addr)}) disconnected.")
+        # print(f"[SERVER] Total connections: {threading.active_count() - 1}")
 
-print("[STARTING] Server is starting...")
+    def update_clients(self, msg):
+        for conn in self.connections:
+            conn.send(msg)
+
+    def print_running_threads(self):
+        for thread in threading.enumerate():
+            print(thread.name)
+
+    def close_all_connections(self):
+        for connection in self.connections:
+            connection.close()
+
+print("[SERVER] Server is starting...")
 Server().start()
